b953f15ca564adc379030431e81c28b2c94dfc9b
Maksym Yaremko <maksymyaremko0202@gmail.com>
2022-12-24 14:26:20 +0300

    Initial commit

---
+++ Source/Citadel/CitadelGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CitadelGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CITADEL_API ACitadelGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
